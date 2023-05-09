e6f97c2f8aeaa1ea004883d33300f2a8d9a177f4
Steven Veld <sveld03@gmail.com>
2023-05-09 13:37:25 -0700

    dummy noun-verb classification

--- MLcodeTest.py
+++ MLcodeTest.py
@@ -1,28 +1,16 @@
-# this is a little bit of Python code from another project I started, just using this as a test
+import nltk
 
-import random
+def word_type(word):
+    pos = nltk.pos_tag([word])
+    if pos[0][1] in ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']:
+        return 'verb'
+    elif pos[0][1] in ['NN', 'NNS', 'NNP', 'NNPS']:
+        return 'noun'
+    else:
+        return 'unknown'
 
-# Represents the properties of the environment
-class Environment (object):
-    
-    # Initializes all environmental factors: temperature, disease, cold, and hot
-    def __init__(self, temperature, disease):
-        # self.temperature = random(50, 90)
-        # self.disease = random(0, 10)
-        self.temperature = temperature
-        self.disease = disease
-        if self.temperature < 70:
-            self.cold = (70 - self.temperature) / 2
-            self.hot = 0
-        elif self.temperature > 70:
-            self.cold = 0
-            self.hot = (self.temperature -  70) / 2
-        else:
-            self.cold = 0
-            self.hot = 0
-
-    def initializeEnvironment():
-        temperature = random(50, 90)
-        disease = random(0, 10)
-        environment = Environment(temperature, disease)
-        return environment
+# Example usage
+print(word_type('run'))  # Output: verb
+print(word_type('house'))  # Output: noun
+print(word_type('apple'))  # Output: noun
+print(word_type('jumping'))  # Output: verb
